85dd6cf7c8a7419586146df58f47a26aa9f18d08
Juan Francisco Pineda <jfpinedap@unal.edu.co>
2021-07-14 01:04:39 -0500

    Feature: create CityInfo and Weather components.

---
+++ src/components/Weather/Weather.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Weather from './Weather'
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+
+test("Weather render", async () => {
+
+    const { findByRole } = render(<Weather temperature={10} />)
+
+    const temp = await findByRole("heading")
+
+    expect(temp).toHaveTextContent("10")
+
+})
